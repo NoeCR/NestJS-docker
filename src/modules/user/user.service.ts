b7e6f143046883385c5303e94db733c7672b849c
NoeCR <noecr.dev@gmail.com>
2020-08-17 18:54:22 +0200

    Removed mapper service
    Signed-off-by: NoeCR <noecr.dev@gmail.com>

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,7 +5,6 @@ import {
 } from "@nestjs/common";
 import { UserRepository } from "./user.repository";
 import { InjectRepository } from "@nestjs/typeorm";
-import { MapperService } from "../../shared/mapper.service";
 import { UserDto } from "./dto/user.dto";
 import { User } from "./user.entity";
 import { UserDetails } from "./user.details.entity";
@@ -17,10 +16,9 @@ export class UserService {
   constructor(
     @InjectRepository(UserRepository) private readonly _userRepository:
       UserRepository,
-    private readonly _mapperService: MapperService,
   ) {}
 
-  async get(id: number): Promise<UserDto> {
+  async get(id: number): Promise<User> {
     if (!id) {
       throw new BadRequestException("id must be send");
     }
@@ -34,21 +32,18 @@ export class UserService {
       throw new NotFoundException();
     }
 
-    return this._mapperService.map<User, UserDto>(user, new UserDto());
+    return user;
   }
 
-  async getAll(): Promise<UserDto[]> {
+  async getAll(): Promise<User[]> {
     const users: User[] = await this._userRepository.find(
       { where: { status: "ACTIVE" } },
     );
 
-    return this._mapperService.mapCollection<User, UserDto>(
-      users,
-      new UserDto(),
-    );
+    return users;
   }
 
-  async create(user: User): Promise<UserDto> {
+  async create(user: User): Promise<User> {
     const details = new UserDetails();
     user.details = details;
 
@@ -59,7 +54,7 @@ export class UserService {
 
     const savedUser = await this._userRepository.save(user);
 
-    return this._mapperService.map<User, UserDto>(savedUser, new UserDto());
+    return savedUser;
   }
 
   async update(id: number, user: User): Promise<void> {
